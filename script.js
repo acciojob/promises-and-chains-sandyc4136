fa79a5793a83752c7a5685bf9c41d36f58e94200
Sandip Chhetry <sandyc4136@gmail.com>
2023-04-01 12:53:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ let age=document.getElementById('age');
 let name=document.getElementById('name');
 let btn=document.getElementById('btn');
 
-function myPromise(e)=new Promise((resolve,reject)=>{
-	e.preventDefault();
+function mainfunc((event)=>{
+	event.preventDefault();
+	
+	var myPromise=new Promise((resolve,reject)=>{
 	setTimeout(()=>{
+		if (!(age && name)) {
+     alert("Please enter valid details");
+			return;
+		   }
 		if(age.value>18){
-			resolve(()=>alert("Welcome `${name.value}`. You can vote."))
+			resolve();
 		}
 		else
-		reject(()=>alert("Oh sorry `${name.value}`. You aren't old enough."))
+		reject()
 	},4000);
 })
+myPromise.then(()=>{
+	alert(`Welcome, ${nameInput.value}. You can vote.`);
+}).catch(()=>{
+	alert("Oh sorry `${name.value}`. You aren't old enough.");
+})
+}
 
-btn.addEventListener('submit',myPromise);
+btn.addEventListener('submit',mainfunc);
 // myPromise(age.value,name.value);
