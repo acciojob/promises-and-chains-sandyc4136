031681bb85ff024493da4dc3064a1f1fce04240c
Sandip Chhetry <sandyc4136@gmail.com>
2023-04-01 12:41:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let age=document.getElementById('age');
+let name=document.getElementById('name');
+let btn=document.getElementById('btn');
+
+function myPromise(e)=new Promise((resolve,reject)=>{
+	e.preventDefault();
+	setTimeout(()=>{
+		if(age.value>18){
+			resolve(()=>alert("Welcome `${name.value}`. You can vote."))
+		}
+		else
+		reject(()=>alert("Oh sorry `${name.value}`. You aren't old enough."))
+	},4000);
+})
+
+btn.addEventListener('submit',myPromise);
+// myPromise(age.value,name.value);
